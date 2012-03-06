0f18eb5605cd6cf7e7091a80619a85fe0f4b1b1b
Yiye Jin <Yiye Jin@10.30.145.159>
2012-03-05 22:11:28 -0800

    change login page from "profile.jsp" to "homepage.jsp"

--- src/quizweb/accountmanagement/LoginServlet.java
+++ src/quizweb/accountmanagement/LoginServlet.java
@@ -46,7 +46,7 @@ public class LoginServlet extends HttpServlet {
 			HttpSession session = request.getSession();
 			User user = new User(username, "1");
 			session.setAttribute("user", user);
-			RequestDispatcher dispatch = request.getRequestDispatcher("profile.jsp");
+			RequestDispatcher dispatch = request.getRequestDispatcher("homepage.jsp");
 			dispatch.forward(request, response);
 		}
 		else {
