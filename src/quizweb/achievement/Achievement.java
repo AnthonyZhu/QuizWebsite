68a0df59788c69f603a24fc6fdd25a42d2495f66
Yizhou Zhu <antonyzyz@gmail.com>
2012-03-06 16:37:37 -0800

    update achievement package

--- src/quizweb/achievement/Achievement.java
+++ src/quizweb/achievement/Achievement.java
@@ -1,25 +1,21 @@
 package quizweb.achievement;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
-
 import quizweb.*;
 import quizweb.database.DBConnection;
-import quizweb.record.AchievementRecord;
 
 public class Achievement {
 	private String description;
 	private String name;
-	private String fileDirectory;
-	
-	
-	
+	private String fileDirectory;	
 	public double threshold;
 	public static ArrayList<Achievement> allAchievements;
-	
-	
-	
+	private static final String DBTable1 = "achievement";
+	private static final String DBTable2 = "achievement_history";
+		
 	public Achievement(String name, String fileDirectory, String discription){
 		this.name = name;
 		this.fileDirectory = fileDirectory;
@@ -38,44 +34,41 @@ public class Achievement {
 		return description;
 	}
 	
-	public static ArrayList<Achievement> getAchievementsByUserID(int UserID) {
-		// TODO initialize achievements from database;
-		DBConnection db = new DBConnection();
-		DBQueries query = new DBQueries();
-		String statement = query.SelectQueryByColumn("achievement id", "Achievement record", "uid=\"" + UserID + "\"");
-		ResultSet rs = db.DBQuery(statement);
-		ArrayList<Achievement> allAchievements = new ArrayList<Achievement>();
-		
-		try{
-			while(rs.next()){
-				String stat = query.SelectAllQuery("Achievement", "achievement id=\"" + rs.getString("achievement id") + "\"");
-				ResultSet rsAchievement = db.DBQuery(stat);
-				while(rsAchievement.next()){
-					String name = rsAchievement.getString("name");
-					String file = rsAchievement.getString("file directory");
-					String discription = rsAchievement.getString("discription");
-					Achievement eachAchievement = new Achievement(name,file,discription);
-					allAchievements.add(eachAchievement);
-				}
-			}
-		}catch(SQLException e){
-			e.printStackTrace();
+	public static void AddNewAchievement(String name, String file, String description){
+		try {
+			String statement = new String("INSERT INTO " + DBTable1 +" VALUES (?, ?, ?)");
+			PreparedStatement stmt = DBConnection.con.prepareStatement(statement);
+			stmt.setString(1, name);
+			stmt.setString(2, file);
+			stmt.setString(3, description);
+			DBConnection.DBUpdate(stmt);
+		} catch (SQLException e1) {
+			e1.printStackTrace();
 		}
-		return allAchievements;
 	}
 	
-	public static void addAchievement(int UserID, Achievement newAchievement){
-		DBConnection db = new DBConnection();
-		DBQueries query = new DBQueries();
-		HashMap<String, String> valueMap = new HashMap<String, String>();
-		valueMap.put("name", newAchievement.getName());
-		valueMap.put("file directory", newAchievement.getFileDirectory());
-		valueMap.put("description", newAchievement.getDescription());
-		String statement = query.InsertQueryByColumn("Achievement", valueMap);
-		db.DBUpdate(statement);
+	public static ArrayList<Achievement> getAchievementsByUserID(int userID) {
+		ArrayList<Achievement> AllAchievements = new ArrayList<Achievement>();
+		try {
+			String statement1 = new String("SELECT * FROM " + DBTable2 +" WHERE uid = ?");
+			PreparedStatement stmt1 = DBConnection.con.prepareStatement(statement1);
+			stmt1.setInt(1, userID);
+			ResultSet rs1 = DBConnection.DBQuery(stmt1);
+			rs1.beforeFirst();
+			while(rs1.next()) {
+				int aid = rs1.getInt("aid");
+				String statement2 = new String("SELECT * FROM " + DBTable1 +" WHERE aid = ?");
+				PreparedStatement stmt2 = DBConnection.con.prepareStatement(statement2);
+				stmt2.setInt(1, aid);
+				ResultSet rs2 = DBConnection.DBQuery(stmt2);
+				Achievement a = new Achievement(rs2.getString("name"), rs2.getString("file"), rs2.getString("description"));
+				AllAchievements.add(a); 
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		return AllAchievements;
 	}
-	
-	
 	
 	public boolean isAccomplished(User user) {
 		System.out.println("THIS LINE SHOULD NEVER BE REACHED");
