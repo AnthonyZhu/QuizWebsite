907665675969d0d094d003be2fa565638d7d1606
Yiye Jin <Yiye Jin@10.30.67.100>
2012-03-15 03:27:26 -0700

    update

--- src/quizweb/announcement/announcementSortByTime.java
+++ src/quizweb/announcement/announcementSortByTime.java
@@ -7,9 +7,9 @@ public class announcementSortByTime implements Comparator<Announcement> {
 	@Override
 	public int compare(Announcement announcement1, Announcement announcement2) {
 		if (announcement1.timestamp.getTime() < announcement2.timestamp.getTime())
-			return -1;
-		else if (announcement1.timestamp.getTime() > announcement2.timestamp.getTime())
 			return 1;
+		else if (announcement1.timestamp.getTime() > announcement2.timestamp.getTime())
+			return -1;
 		else 
 			return 0;
 	}
